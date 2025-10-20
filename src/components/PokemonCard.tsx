7a668c63a9e45415ad3e4f834dcf494fe6b69af0
Drakiinoxx <nicolas.malafosse@gmail.com>
2025-10-20 22:32:58 +0200

    pokemon

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,26 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[1];
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="pokemon"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      <figcaption>{pokemon.name}</figcaption>
+      {pokemon.imgSrc !== undefined ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
     </figure>
   );
 }
+
 export default PokemonCard;
