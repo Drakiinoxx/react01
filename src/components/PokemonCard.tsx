a4c190a7c8e3ef0de2d0f29d714a53b6920388ed
Drakiinoxx <nicolas.malafosse@gmail.com>
2025-10-20 23:17:28 +0200

    change

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,11 @@
-function PokemonCard({ pokemon }) {
+interface PokemonProps {
+  pokemon: {
+    name: string;
+    imgSrc?: string;
+  };
+}
+
+function PokemonCard({ pokemon }: PokemonProps) {
   return (
     <figure>
       <figcaption>{pokemon.name}</figcaption>
