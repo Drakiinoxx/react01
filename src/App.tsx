cd4a45c0d99c3053a4c789520471ee414a75ca4d
Drakiinoxx <nicolas.malafosse@gmail.com>
2025-10-21 15:34:58 +0200

    help me j'ai la tete en feu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-
+import NavBar from "./components/navbar";
 import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
@@ -40,16 +40,7 @@ function App() {
 
   return (
     <div>
-      <nav>
-        {pokemonList.map((pokemon) => (
-          <button
-            key={pokemon.name}
-            onClick={() => setPokemonName(pokemon.name)}
-          >
-            {pokemon.name}
-          </button>
-        ))}
-      </nav>
+      <NavBar setPokemonName={setPokemonName} pokemonList={pokemonList} />
       <PokemonCard pokemon={pokemon} />
     </div>
   );
